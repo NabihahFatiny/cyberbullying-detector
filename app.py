3f79c40eccda66e36de519c5b268e9d4a0de8783
NabihahFatiny <nurnabihahfatiny545@gmail.com>
2026-05-06 03:41:45 +0800

    testtt

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Cyberbullying Detector is working!"
+    return render_template('index.html')
 
 @app.route('/test')
 def test():
